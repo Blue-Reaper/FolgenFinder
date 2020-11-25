5b57f00d02e9bbb94a8b9c8c264ce2b39b81407b
Blue-Reaper <46467587+Blue-Reaper@users.noreply.github.com>
2020-11-25 08:34:09 +0100

    get id of specific folder in bookmarks

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,1 +1,39 @@
-document.getElementById('myHeading').style.color = 'red'
+browserAction();
+
+function browserAction() {
+  var defaultId = getDefaultFolderId();
+  defaultId.then((id) => console.log('Id: ' + id));
+}
+
+function getDefaultFolderId() {
+  //idea set folder name in settings and only default is "FolgenFinder"
+  return Promise.resolve()
+    .then(() => browser.bookmarks.search({ title: 'FolgenFinder' }))
+    .then((result) => {
+      if (result.length == 0) {
+        console.log('Default folder not found.');
+        // create default folder if not exists
+        return Promise.resolve()
+          .then(function () {
+            return browser.bookmarks.create({
+              title: 'FolgenFinder',
+            });
+          })
+          .then(function (result) {
+            return result.id;
+          });
+      } else if (result.length > 1) {
+        //   ToDo add error handling
+        console.error('More than one folder "' + result[0].title + '" found.');
+      } else {
+        return result[0].id;
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        console.error(err);
+      }
+
+      return Promise.resolve();
+    });
+}
